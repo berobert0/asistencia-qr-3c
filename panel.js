2136ab628b9df7ed73432d75397a23049f388568
berobert0 <54590606+berobert0@users.noreply.github.com>
2026-04-25 02:55:30 -0500

    panel.js

--- panel.js
+++ panel.js
@@ -1,20 +1,45 @@
-const API = "https://script.google.com/macros/s/AKfycbzvykMTRDuoV3q-E4ArxOjrVBLRWgaYK1yDo94NfWAf9waBWbM0vaFML1r3JzpATYIb/exec";
+const API="https://script.google.com/macros/s/AKfycbwn8J_sZ0MRDzKrDefOKUCJ9sTzOJ0h2MGDYAzu-DpjN4WXz0bSlA7Jm-vSjR0bJm7L/exec";
 
 let chart;
 
+function login(){
+
+fetch(API+
+"?tipo=login"+
+"&user="+user.value+
+"&pass="+pass.value)
+
+.then(r=>r.json())
+.then(d=>{
+
+ if(d.ok){
+
+   document.getElementById("login")
+   .style.display="none";
+
+   document.getElementById("panel")
+   .style.display="block";
+
+   cargar();
+
+ }else{
+   alert("Credenciales incorrectas");
+ }
+
+});
+
+}
+
 function cargar(){
 
-fetch(API + "?tipo=panel")
-.then(r => r.json())
-.then(data => {
+fetch(API+"?tipo=panel")
+.then(r=>r.json())
+.then(data=>{
 
-let a = 0;
-let t = 0;
-let f = 0;
+let a=0,t=0,f=0;
+let html="";
 
-let html = "";
-
-data.reverse().forEach(x => {
+data.reverse().forEach(x=>{
 
  if(x.estado=="ASISTENCIA") a++;
  if(x.estado=="TARDANZA") t++;
@@ -30,11 +55,11 @@ data.reverse().forEach(x => {
 
 });
 
-tabla.innerHTML = html;
+tabla.innerHTML=html;
 
-asis.innerHTML = a;
-tard.innerHTML = t;
-falt.innerHTML = f;
+asis.innerHTML=a;
+tard.innerHTML=t;
+falt.innerHTML=f;
 
 if(chart) chart.destroy();
 
@@ -54,6 +79,29 @@ data:[a,t,f]
 
 }
 
-cargar();
+function exportar(){
 
-setInterval(cargar,5000);
+let tablaHTML =
+document.querySelector("table")
+.outerHTML;
+
+let blob = new Blob(
+[tablaHTML],
+{type:"application/vnd.ms-excel"}
+);
+
+let a = document.createElement("a");
+
+a.href = URL.createObjectURL(blob);
+
+a.download="reporte.xls";
+
+a.click();
+
+}
+
+setInterval(()=>{
+ if(panel.style.display=="block"){
+   cargar();
+ }
+},5000);
