f6dcc095b456649ebf0cc1ccbc569aa2ac1d117c
berobert0 <54590606+berobert0@users.noreply.github.com>
2026-04-25 02:56:02 -0500

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbzvykMTRDuoV3q-E4ArxOjrVBLRWgaYK1yDo94NfWAf9waBWbM0vaFML1r3JzpATYIb/exec";
+const API = "https://script.google.com/macros/s/AKfycbwn8J_sZ0MRDzKrDefOKUCJ9sTzOJ0h2MGDYAzu-DpjN4WXz0bSlA7Jm-vSjR0bJm7L/exec";
 
 let bloqueado = false;
 
