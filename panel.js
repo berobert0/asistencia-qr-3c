9513f06ed32c1f3d3cf523fd237eea6556b23bc5
berobert0 <54590606+berobert0@users.noreply.github.com>
2026-05-11 14:03:43 -0500

    panel.js

--- panel.js
+++ panel.js
@@ -1,161 +1,58 @@
-const API="https://script.google.com/macros/s/AKfycbyPbslr15y2s-jxxh5xUJ1PPw2ruyMp0pwI8sT8XXH8-dCTIn6_elkWc8Mla1tS7Lg/exec";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyPbslr15y2s-jxxh5xUJ1PPw2ruyMp0pwI8sT8XXH8-dCTIn6_elkWc8Mla1tS7Lg/exec";
 
-let chartBar;
-let chartPie;
-
-function login(){
+function cargar(){
 
   fetch(
-
-    API+
-
-    "?tipo=login"+
-
-    "&user="+
-    encodeURIComponent(user.value)+
-
-    "&pass="+
-    encodeURIComponent(pass.value)
-
+    API_URL + "?tipo=panel"
   )
 
   .then(r=>r.json())
 
-  .then(d=>{
+  .then(data=>{
 
-    if(d.ok){
+    let html = "";
 
-      login.style.display="none";
+    data.reverse().forEach(d=>{
 
-      panel.style.display="block";
+      html += `
 
-      cargar();
+      <tr>
 
-    }else{
+        <td>${d.codigo}</td>
 
-      alert("Credenciales incorrectas");
+        <td>${d.nombre}</td>
 
-    }
+        <td>${d.grado}</td>
+
+        <td>${d.seccion}</td>
+
+        <td>${d.fecha}</td>
+
+        <td>${d.hora}</td>
+
+        <td>${d.estado}</td>
+
+      </tr>
+
+      `;
+
+    });
+
+    document.getElementById(
+      "tabla"
+    ).innerHTML = html;
+
+  })
+
+  .catch(err=>{
+
+    console.error(err);
 
   });
 
 }
 
+setInterval(cargar,3000);
 
-function cargar(){
-
-fetch(API+"?tipo=panel")
-
-.then(r=>r.json())
-
-.then(data=>{
-
-let resumen={
-ASISTENCIA:0,
-TARDANZA:0,
-FALTA:0
-};
-
-let html="";
-
-data.reverse().forEach(d=>{
-
-if(resumen[d.estado]!=null){
-
- resumen[d.estado]++;
-
-}
-
-html+=`
-
-<tr>
-
-<td>${d.nombre}</td>
-
-<td>${d.grado}</td>
-
-<td>${d.seccion}</td>
-
-<td>${d.hora}</td>
-
-<td>${d.estado}</td>
-
-</tr>
-
-`;
-
-});
-
-tabla.innerHTML=html;
-
-asis.innerHTML=
-resumen.ASISTENCIA;
-
-tard.innerHTML=
-resumen.TARDANZA;
-
-falt.innerHTML=
-resumen.FALTA;
-
-graficos(resumen);
-
-});
-
-}
-
-
-function graficos(r){
-
-const data=[
-r.ASISTENCIA,
-r.TARDANZA,
-r.FALTA
-];
-
-if(chartBar) chartBar.destroy();
-if(chartPie) chartPie.destroy();
-
-chartBar=new Chart(
-
-document.getElementById("graficoBarras"),
-
-{
-type:'bar',
-data:{
-labels:[
-'Asistencia',
-'Tardanza',
-'Falta'
-],
-datasets:[{data:data}]
-}
-});
-
-chartPie=new Chart(
-
-document.getElementById("graficoPastel"),
-
-{
-type:'pie',
-data:{
-labels:[
-'Asistencia',
-'Tardanza',
-'Falta'
-],
-datasets:[{data:data}]
-}
-});
-
-}
-
-
-setInterval(()=>{
-
-if(panel.style.display==="block"){
-
- cargar();
-
-}
-
-},5000);
+cargar();
