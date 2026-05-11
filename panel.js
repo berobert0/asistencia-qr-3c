697831d7c63252c9e70c5b0c1379f703a002835f
berobert0 <54590606+berobert0@users.noreply.github.com>
2026-05-11 10:13:53 -0500

    panel.js

--- panel.js
+++ panel.js
@@ -1,66 +1,78 @@
-const API="https://script.google.com/macros/s/AKfycbwb_k8JvvmzldaPk-D-8uHPg7craueCyK-pcM61Vy2s46slSvA3LzWr8yh-vldJK0gq/exec";
+const API =
+"https://script.google.com/macros/s/AKfycbwb_k8JvvmzldaPk-D-8uHPg7craueCyK-pcM61Vy2s46slSvA3LzWr8yh-vldJK0gq/exec";
 
 let chart;
 
+
+/****************************************
+ LOGIN
+****************************************/
 function login(){
 
-fetch(API+
-"?tipo=login"+
-"&user="+user.value+
-"&pass="+pass.value)
+  const usuario =
+  document.getElementById("user").value;
 
-.then(r=>r.json())
-.then(d=>{
+  const clave =
+  document.getElementById("pass").value;
 
- if(d.ok){
+  fetch(
+    API +
+    "?tipo=login" +
+    "&user=" + encodeURIComponent(usuario) +
+    "&pass=" + encodeURIComponent(clave)
+  )
 
-   document.getElementById("login")
-   .style.display="none";
+  .then(r => r.json())
 
-   document.getElementById("panel")
-   .style.display="block";
+  .then(d => {
 
-   cargar();
-  function resumenMensual(){
+    if(d.ok){
 
-fetch(API+"?tipo=panel")
-.then(r=>r.json())
-.then(data=>{
+      document.getElementById("login")
+      .style.display = "none";
 
- let a=0,t=0,f=0;
+      document.getElementById("panel")
+      .style.display = "block";
 
- data.forEach(x=>{
+      cargar();
 
-  if(x.estado=="ASISTENCIA") a++;
-  if(x.estado=="TARDANZA") t++;
-  if(x.estado=="FALTA") f++;
+    }else{
 
- });
+      alert("Credenciales incorrectas");
 
- console.log("Mensual",a,t,f);
+    }
 
-});
+  })
 
-}
+  .catch(err => {
 
- }else{
-   alert("Credenciales incorrectas");
- }
+    console.log(err);
 
-});
+    alert("Error conexión");
+
+  });
 
 }
 
+
+/****************************************
+ CARGAR PANEL
+****************************************/
 function cargar(){
 
-fetch(API+"?tipo=panel")
-.then(r=>r.json())
-.then(data=>{
+fetch(API + "?tipo=panel")
 
-let a=0,t=0,f=0;
+.then(r => r.json())
+
+.then(data => {
+
+let a=0;
+let t=0;
+let f=0;
+
 let html="";
 
-data.reverse().forEach(x=>{
+data.reverse().forEach(x => {
 
  if(x.estado=="ASISTENCIA") a++;
  if(x.estado=="TARDANZA") t++;
@@ -68,61 +80,105 @@ data.reverse().forEach(x=>{
 
  html += `
  <tr>
- <td>${x.nombre}</td>
- <td>${x.hora}</td>
- <td>${x.estado}</td>
+   <td>${x.nombre}</td>
+   <td>${x.hora}</td>
+   <td>${x.estado}</td>
  </tr>
  `;
 
 });
 
-tabla.innerHTML=html;
+document.getElementById("tabla").innerHTML = html;
 
-asis.innerHTML=a;
-tard.innerHTML=t;
-falt.innerHTML=f;
+document.getElementById("asis").innerHTML = a;
+document.getElementById("tard").innerHTML = t;
+document.getElementById("falt").innerHTML = f;
 
-if(chart) chart.destroy();
+
+/****************************************
+ GRAFICO
+****************************************/
+if(chart){
+
+ chart.destroy();
+
+}
 
 chart = new Chart(
+
 document.getElementById("grafico"),
+
 {
-type:"bar",
-data:{
-labels:["Asistencia","Tardanza","Falta"],
-datasets:[{
-data:[a,t,f]
-}]
-}
-});
+ type:"bar",
+
+ data:{
+
+  labels:[
+   "Asistencia",
+   "Tardanza",
+   "Falta"
+  ],
+
+  datasets:[{
+
+   data:[a,t,f]
+
+  }]
+
+ }
 
 });
 
+})
+
+.catch(err => {
+
+ console.log(err);
+
+});
+
 }
 
+
+/****************************************
+ EXPORTAR EXCEL
+****************************************/
 function exportar(){
 
 let tablaHTML =
-document.querySelector("table")
-.outerHTML;
+document.querySelector("table").outerHTML;
 
 let blob = new Blob(
-[tablaHTML],
-{type:"application/vnd.ms-excel"}
+
+ [tablaHTML],
+
+ {type:"application/vnd.ms-excel"}
+
 );
 
 let a = document.createElement("a");
 
 a.href = URL.createObjectURL(blob);
 
-a.download="reporte.xls";
+a.download = "reporte.xls";
 
 a.click();
 
 }
 
-setInterval(()=>{
- if(panel.style.display=="block"){
+
+/****************************************
+ ACTUALIZAR AUTOMÁTICO
+****************************************/
+setInterval(() => {
+
+ if(
+  document.getElementById("panel")
+  .style.display == "block"
+ ){
+
    cargar();
+
  }
+
 },5000);
